bea13326b932c8b5397e8814b862130c680f736f
NavyCMG <CMGrimes@student.fullsail.edu>
2024-09-29 16:40:45 -0400

    Add project files.

---
+++ Source/EquipmentPlugin/EquipmentPluginGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "EquipmentPluginGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AEquipmentPluginGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AEquipmentPluginGameMode();
+};
+
+
+
